ffe020c1f3f4c72f345645dc9578907c02158484
Erik <xxxsmoizxxx@gmail.com>
2015-12-15 22:15:34 +0100

    Added autoupdate

--- dokku-backup.sh
+++ dokku-backup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# update to latests version
+git pull origin
+
+# set variables
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 tmp=$(mktemp -d)
 
+# do backup
 dokku postgres:export treachery2 > $tmp"/treachery2_postgres.bak"
 dropbox_uploader.sh upload $tmp"/treachery2_postgres.bak" "treachery/postgres_"$current_time".bak"
 
+# clean up
 rm -rf $tmp
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 echo "Setting up requirements..."
 git clone https://github.com/andreafabrizi/Dropbox-Uploader.git /opt/
-ln -s /opt/Dropbox-Uploader/dropbox_uploader.sh /usr/local/bin/
+ln -s /opt/Dropbox-Uploader/dropbox_uploader.sh /usr/local/bin/dropbox_uploader.sh
 
 echo "Configuring requirements"
 /usr/local/bin/dropbox_uploader.sh
