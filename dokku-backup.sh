70f9c1f0c1a7ed45172f84fd8eb1c0d11c8e4bfd
Erik <xxxsmoizxxx@gmail.com>
2015-12-15 22:36:23 +0100

    fix: Bug

--- dokku-backup.sh
+++ dokku-backup.sh
@@ -4,7 +4,7 @@
 # $2 - name
 # $3 - type
 dokku_backup_database() {
-  $tmp=$(mktemp)
+  tmp=$(mktemp)
   dokku $1 > $tmp
   dropbox_uploader.sh upload $tmp $2"/"$3"/"$current_time".bak"
   rm -f $tmp
@@ -15,7 +15,6 @@ git pull origin
 
 # set variables
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-tmp=$(mktemp -d)
 
 # do backup
 dokku_backup_database "dokku postgres:export treachery2" "treachery" "database"
