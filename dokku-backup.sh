2fde3ac7422c5407b446ab4871109f23b52c821d
Erik <xxxsmoizxxx@gmail.com>
2015-12-15 22:08:34 +0100

    fix: Erronous dots

--- dokku-backup.sh
+++ dokku-backup.sh
@@ -3,7 +3,7 @@
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 tmp=$(mktemp -d)
 
-dokku postgres:export treachery2 > $tmp."/treachery2_postgres.bak"
-dropbox_uploader.sh upload $tmp."/treachery2_postgres.bak" "treachery/postgres_".$current_time.".bak"
+dokku postgres:export treachery2 > $tmp"/treachery2_postgres.bak"
+dropbox_uploader.sh upload $tmp"/treachery2_postgres.bak" "treachery/postgres_"$current_time".bak"
 
 rm -rf $tmp
