0f5c56efcd8d8d1a9ddda67d19971c79bd9984d7
Erik Gärtner <erik@gartner.io>
2016-06-30 08:07:03 +0200

    feat: Add opt-in option for notifying using pushover

--- dokku-backup-lib.sh
+++ dokku-backup-lib.sh
@@ -5,11 +5,14 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 # $1 - dokku command
 # $2 - name
 # $3 - type
+# $4 - report to pushover
 dokku_backup_database() {
   tmp=$(mktemp)
   dokku $1 > $tmp
   msg=$(dropbox_uploader.sh -f $DB_CONFIG_PATH upload $tmp $2"/"$3"/"$current_time".bak")
-  pushover.sh -T $PO_TOKEN -U $PO_USER -t "Dokku-backup: "$2 $msg
+  if [ $4 -eq 1 ]; then
+    pushover.sh -T $PO_TOKEN -U $PO_USER -t "Dokku-backup: "$2 $msg
+  fi
   echo $msg
   rm -f $tmp
 }
