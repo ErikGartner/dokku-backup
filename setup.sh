45f165d22caf837d72591600869e5880cc0e47b9
Erik Gärtner <ErikGartner@users.noreply.github.com>
2016-10-16 22:57:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Setting up requirements..."
-git clone https://github.com/andreafabrizi/Dropbox-Uploader.git /opt/
+git clone https://github.com/andreafabrizi/Dropbox-Uploader.git /opt/Dropbox-Uploader
 ln -s /opt/Dropbox-Uploader/dropbox_uploader.sh /usr/local/bin/dropbox_uploader.sh
 
 git clone https://github.com/jnwatts/pushover.sh.git /opt/pushover
