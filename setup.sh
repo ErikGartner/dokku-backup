f15096c8829c0788a19b544df345e23c1c8e8cda
Erik Gärtner <ErikGartner@users.noreply.github.com>
2018-01-28 13:17:51 +0100

    Simplify setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Setting up requirements..."
-git clone https://github.com/andreafabrizi/Dropbox-Uploader.git /opt/Dropbox-Uploader
-ln -s /opt/Dropbox-Uploader/dropbox_uploader.sh /usr/local/bin/dropbox_uploader.sh
-
-git clone https://github.com/jnwatts/pushover.sh.git /opt/pushover
-chmod +x /opt/pushover/pushover.sh
-ln -s /opt/pushover/pushover.sh /usr/local/bin/pushover.sh
+echo "configuring requirements"
+./Dropbox-Uploader/dropbox_uploader.sh
 
 ln -s ./dokku-backup.sh /usr/local/bin/dokku-backup.sh
-
-echo "Configuring requirements"
-/usr/local/bin/dropbox_uploader.sh
