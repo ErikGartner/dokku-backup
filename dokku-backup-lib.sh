ee33271a053f5e7fc444e4a39000e57054ae1f6e
Erik Gärtner <ErikGartner@users.noreply.github.com>
2018-01-28 13:23:03 +0100

    Update dokku-backup-lib.sh

--- dokku-backup-lib.sh
+++ dokku-backup-lib.sh
@@ -8,6 +8,7 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 # $4 - report to pushover
 dokku_backup_database() {
   tmp=$(mktemp)
+  echo "Backing up $2 $3"
   dokku $1 > $tmp
   msg=$(./Dropbox-Uploader/dropbox_uploader.sh -f $DB_CONFIG_PATH upload $tmp $2"/"$3"/"$current_time".bak")
   if [ "$4" = "1" ]; then
